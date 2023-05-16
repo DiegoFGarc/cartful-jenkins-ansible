9b88bc813109440af8dba5c6daf0a10a2e9feb05
Diego García <diegorayapiso@gmail.com>
2023-05-16 18:04:08 -0500

    Validating stages

--- request.sh
+++ request.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 base_url="https://api-env.cartfulsolutions.com"
-environment="{{ env_name }}"
+environment="staging"
 url="$base_url/$environment/status"
 
 response=$(curl -s -w "%{http_code}" "$url")
 
 # Extraer el código de estado de la respuesta
 status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+if [[ $status_code -eq 200 ]]; then
+    echo "Status code is 200. Pipeline continues."
+else
+    echo "Error: Status code is not 200. Pipeline fails."
+    exit 1
+fi
 
 echo "Server response status code:"
 echo $status_code
